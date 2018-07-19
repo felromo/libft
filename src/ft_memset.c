127c81b2b9262ecc49de116ba55741f80cf9fe4a
Felipe Romo <felipe@fromo.me>
2018-07-18 22:07:52 -0500

    adds ft_memset

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -5,10 +5,9 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*tmp;
 
 	tmp = ((unsigned char *) b);
-	while (*tmp && len > 0)
+	while (len-- > 0)
 	{
 		*tmp++ = (unsigned char)c;
-		len--;
 	}
 
 	return (b);
