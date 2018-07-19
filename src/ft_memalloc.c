a1fe4fc357d37cda3431a2c481eb1236d1b08051
Felipe Romo <felipe@fromo.me>
2018-07-18 23:23:41 -0500

    adds ft_memalloc

--- src/ft_memalloc.c
+++ src/ft_memalloc.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include "libft.h"
 
 /*
 void	*ft_memalloc(size_t size)
@@ -12,6 +13,6 @@ void	*ft_memalloc(size_t size)
 void	*ft_memalloc(size_t size)
 {
 	void *p1 = malloc(size * sizeof(void));
+	bzero(p1, size);
 	return (p1);
 }
-
