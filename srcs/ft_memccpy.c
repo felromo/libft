019ebf2d30b905bb4ab5c5e0995627d048f2e351
Felipe Romo <felipe@fromo.me>
2018-07-23 20:32:49 -0500

    updates ft_memccpy

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -2,5 +2,11 @@
 
 void  *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 {
-  
+  unsigned char delimeter = c;
+  while (n-- > 0)
+    {
+      if (*((char *)src) == delimeter)
+        break;
+    }
+  return dst;
 }
