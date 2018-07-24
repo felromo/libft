72aba044b18e0dcea528fcf669f13ac715838710
Felipe Romo <fromo@e1z3r4p23.42.us.org>
2018-07-24 15:38:45 -0700

    returns correct memory address

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -2,7 +2,9 @@
 
 void  *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-  while (n-- > 0)
-    *(char *)dst++ = *(char *)src++;
+	char	*tmp;
+	tmp = dst;
+	while (n-- > 0)
+		*tmp++ = *(char *)src++;
   return dst;
 }
