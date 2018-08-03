af3e16b2c698e2bb01476ccc6acd5c8880fc6afc
Felipe Romo <fromo@e1z2r3p33.42.us.org>
2018-08-03 16:44:56 -0700

    fixes ft_putnbr

--- srcs/ft_putnbr.c
+++ srcs/ft_putnbr.c
@@ -1,4 +1,19 @@
+#include "libft.h"
+
 void    ft_putnbr(int n)
 {
-    
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + '0');
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
 }
