760175795cb36a1240130a3686ff139c43e25eb2
Felipe Romo <fromo@e1z2r3p33.42.us.org>
2018-08-03 21:30:13 -0700

    adds logic for 1 digit conversion

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -1,4 +1,19 @@
+#include <stdlib.h>
+
 char    *ft_itoa(int n)
 {
-    return (n);
+    
+	char *toa;
+
+	// turn int n into an alpha representation
+	toa = (char *)malloc(sizeof(char) + 1);
+	if ((toa = (char *)malloc(sizeof(char) + 1)))
+	{
+		char *head = toa;
+		*toa = (n + '0');
+		toa++;
+		*toa = '\0';
+		return (head);
+	}
+	return (NULL);
 }
