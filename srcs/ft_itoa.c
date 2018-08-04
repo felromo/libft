4159b7cbc1703832f0a02e542ae63b200188c89f
Felipe Romo <fromo@e1z2r3p33.42.us.org>
2018-08-04 02:57:22 -0700

    updated ft_itoa still not working

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -1,12 +1,21 @@
+#include <libft.h>
 #include <stdlib.h>
 
 char    *ft_itoa(int n)
 {
-    
+	int len;
 	char *toa;
 
+	// some_method = number -> number of place values
+	len = ft_num_len(n);
+
 	// turn int n into an alpha representation
-	toa = (char *)malloc(sizeof(char) + 1);
+	toa = (char *)malloc((sizeof(char) * len) + 1);
+	if (toa)
+	{
+		ft_itoa(n / 10);
+		ft_itoa(n % 10);
+	}
 	if ((toa = (char *)malloc(sizeof(char) + 1)))
 	{
 		char *head = toa;
