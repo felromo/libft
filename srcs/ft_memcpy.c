7687e842ef494affd9ea997aa4f367ff8f934535
Felipe Romo <fromo@e1z2r7p1.42.us.org>
2018-08-19 15:18:30 -0700

    norm updates

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -4,7 +4,7 @@ void  *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	char	*tmp;
 	tmp = dst;
-	while (n-- > 0)
+	while (n--)
 		*tmp++ = *(char *)src++;
-  return dst;
+  return (dst);
 }
