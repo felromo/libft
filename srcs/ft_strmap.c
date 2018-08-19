642ac0c15225390ff15d2ddf85bb338ab0542240
Felipe Romo <fromo@e1z2r7p1.42.us.org>
2018-08-19 15:51:08 -0700

    adds ft_strmap not-protected

---
+++ srcs/ft_strmap.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <libft.h>
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	int		outside_index;
+	int		i;
+	int		len;
+	char	*buf;
+
+	outside_index = 0;
+	i = 0;
+	len = ft_strlen(s);
+	buf = (char *)malloc(sizeof(char) * (len + 1));
+	if (buf)
+	{
+		while (s[outside_index])
+		{
+			buf[outside_index] = (*f)(s[outside_index]);
+			outside_index++;
+		}
+		buf[outside_index] = '\0';
+	}
+	return (buf);
+}
