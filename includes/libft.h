0882421e73f1205e83eb81ef6d85b29f6c71eb98
Felipe Romo <fromo@e1z2r7p1.42.us.org>
2018-08-19 16:12:05 -0700

    adds ft_strequ not-protected

--- includes/libft.h
+++ includes/libft.h
@@ -91,6 +91,7 @@ void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f)(char));
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int     ft_strequ(char const *s1, char const *s2);
 char    *ft_itoa(int n);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
