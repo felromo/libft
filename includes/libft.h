087cebde261b4dddde881540805e2a31027769e8
Felipe Romo <fromo@e1z2r7p1.42.us.org>
2018-08-20 01:26:01 -0700

    adds ft_memcmp

--- includes/libft.h
+++ includes/libft.h
@@ -72,6 +72,7 @@ void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void    *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
