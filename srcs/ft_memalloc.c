7b85617972ea65558f913e13bcb8d95e46636870
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:14:07 -0700

    norm formatting ft_memalloc.c

--- srcs/ft_memalloc.c
+++ srcs/ft_memalloc.c
@@ -1,16 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:13:14 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:13:51 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
 
 void	*ft_memalloc(size_t size)
 {
-	void *p1;
-	
+	void	*p1;
+
 	if ((p1 = malloc(size)) == NULL)
 		return (NULL);
-	// return *((unsigned char *)p1) ? ft_bzero(p1, size) : p1;
 	ft_bzero(p1, size);
 	return (p1);
-	//bzero(p1, size);
-	//return (p1);
 }
