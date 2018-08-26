17269fb48be96921ea4755783c7e44c312c54883
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:28:05 -0700

    norm formatting ft_strjoin.c

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -1,29 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:27:26 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:27:29 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <libft.h>
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char    *buf;
-    int     l1;
-    int     l2;
-    int     i;
+	char	*buf;
+	int		l1;
+	int		l2;
+	int		i;
 
-    l1 = ft_strlen(s1);
-    l2 = ft_strlen(s2);
-    i = 0;
-    buf = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
-    if (buf)
-    {
-        while (*s1)
-        {
-            *buf++ = *s1++;
-        }
-        while (*s2)
-        {
-            *buf++ = *s2++;
-        }
-        *buf = '\0';
-        return (buf - (l1 + l2));
-    }
-    return (buf);
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	i = 0;
+	buf = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
+	if (buf)
+	{
+		while (*s1)
+		{
+			*buf++ = *s1++;
+		}
+		while (*s2)
+		{
+			*buf++ = *s2++;
+		}
+		*buf = '\0';
+		return (buf - (l1 + l2));
+	}
+	return (buf);
 }
