111535ef26b0f0d79804c345da54e7f091fc481a
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 23:55:32 -0700

    norm formatting ft_isalpha.c

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -1,27 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 23:54:09 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 23:54:47 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <ctype.h>
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-  /* if ((c >= 65 && c <= 90) || (c >=97 && c <= 122)) */
-  return ((c >= 65 && c <= 90) || (c >=97 && c <= 122));
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
 }
-// int main(void)
-// {
-//   printf("%c ft_isalpha: %d\n", 65, ft_isalpha(65));
-//   printf("%c isalpha: %d\n", 65, isalpha(65));
-
-//   printf("%c ft_isalpha: %d\n", 66, ft_isalpha(66));
-//   printf("%c isalpha: %d\n", 66, isalpha(66));
-
-//   printf("%c ft_isalpha: %d\n", 94, ft_isalpha(94));
-//   printf("%c isalpha: %d\n", 94, isalpha(94));
-
-//   printf("%c ft_isalpha: %d\n", 1, ft_isalpha(1));
-//   printf("%c isalpha: %d\n", 1, isalpha(1));
-
-//   printf("%c ft_isalpha: %d\n", 10, ft_isalpha(10));
-//   printf("%c isalpha: %d\n", 10, isalpha(10));
-
-//   return (0);
-// }
