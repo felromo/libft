aeb28ed08284adec2a849e6e3784721b522de093
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:26:52 -0700

    norm formatting ft_strsub.c

--- srcs/ft_strsub.c
+++ srcs/ft_strsub.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:26:31 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:26:32 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-char    *ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-    char    *buf;
-    int     i;
+	char	*buf;
+	int		i;
 
-    buf = (char *)malloc(sizeof(char) * (len + 1));
-    i = 0;
-    if (buf)
-    {
-        while (len-- && s[start])
-        {
-            buf[i] = s[start];
-            i++;
-            start++;
-        }
-        buf[i] = '\0';
-    }
-    return (buf);
+	buf = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (buf)
+	{
+		while (len-- && s[start])
+		{
+			buf[i] = s[start];
+			i++;
+			start++;
+		}
+		buf[i] = '\0';
+	}
+	return (buf);
 }
