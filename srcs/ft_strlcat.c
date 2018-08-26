d6b3a56b326e9224326c1cfcbe58959818d73f2c
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 23:02:10 -0700

    norm formatting

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -1,30 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 22:59:56 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 23:01:52 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <libft.h>
 
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-  char *d = dst;
-  const char *s = src;
-  size_t n = dstsize;
-  size_t dlen;
+	char		*d;
+	const char	*s;
+	size_t		n;
+	size_t		dlen;
 
-  while (n-- != 0 && *d != '\0')
-    d++;
-  dlen = d - dst;
-  n = dstsize - dlen;
-
-  if (n == 0)
-    return (dlen + ft_strlen(s));
-
-  while (*s != '\0')
-    {
-      if (n != 1)
-        {
-          *d++ = *s;
-          n--;
-        }
-      s++;
-    }
-  *d = '\0';
-  return (dlen + (s - src));
+	d = dst;
+	s = src;
+	n = dstsize;
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = dstsize - dlen;
+	if (n == 0)
+		return (dlen + ft_strlen(s));
+	while (*s != '\0')
+	{
+		if (n != 1)
+		{
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+	return (dlen + (s - src));
 }
