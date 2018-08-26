4aeb05158f68182b6e65a55ae2d52a28edc2bb7f
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 22:41:50 -0700

    norm formatting

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 22:40:07 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 22:40:08 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-void  *ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char delimeter = c;
+	unsigned char	delimeter;
+
+	delimeter = c;
 	while (n-- > 0)
 	{
 		*(unsigned char *)dst++ = *(unsigned char *)src;
@@ -10,5 +24,5 @@ void  *ft_memccpy(void *dst, const void *src, int c, size_t n)
 			return ((unsigned char *)dst);
 		src++;
 	}
-	return NULL;
+	return (NULL);
 }
