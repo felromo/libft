e079c9ebc84dfb0abe607f52a78ebdc0ac25263e
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:26:06 -0700

    norm formatting ft_strnequ.c

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:25:39 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:25:40 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int     ft_strnequ(char const *s1, char const *s2, size_t n)
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-    while (*s1 && *s2 && n--)
-    {
-        if (*s1++ != *s2++)
-            return (0);
-    }
-    return (*s1 == *s2);
+	while (*s1 && *s2 && n--)
+	{
+		if (*s1++ != *s2++)
+			return (0);
+	}
+	return (*s1 == *s2);
 }
