f68a9c86df1d64fcbe37ff0be9b48ab5e644b5dd
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:29:55 -0700

    norm formatting ft_strtrim.c

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -1,21 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:28:25 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:29:38 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <libft.h>
 
-char    *ft_strtrim(char const *s)
+char	*ft_strtrim(char const *s)
 {
-    int     start;
-    int     end;
-    int     len;
-    char    *str;
-    char    *buf;
+	int		start;
+	int		end;
+	int		len;
+	char	*str;
+	char	*buf;
 
-    start = 0;
-    end = 0; 
-    len = ft_strlen(s);
-    while (ft_iswhitespace(s[start++]));
-    str = (char *)s + (ft_strlen(s) - 1);
-    while (ft_iswhitespace(*str--))
-        end++;
-    buf = ft_strsub(s, start - 1, (len - ((start - 1) + (end))));
-    return (--start == len ? "" : buf);
+	start = 0;
+	end = 0;
+	len = ft_strlen(s);
+	while (ft_iswhitespace(s[start++]))
+		;
+	str = (char *)s + (ft_strlen(s) - 1);
+	while (ft_iswhitespace(*str--))
+		end++;
+	buf = ft_strsub(s, start - 1, (len - ((start - 1) + (end))));
+	return (--start == len ? "" : buf);
 }
