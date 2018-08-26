ffd3c0f45ee467c0c1927eae4c433159eec3555c
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-26 00:24:02 -0700

    norm formatting ft_strmapi.c

--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -1,24 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/26 00:21:36 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/26 00:23:16 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <libft.h>
 
-
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char ))
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 {
-    int     outside_index;
-    int     len;
-    char    *buf;
+	int		outside_index;
+	int		len;
+	char	*buf;
 
-    outside_index = 0;
-    len = ft_strlen(s);
-    buf = (char *)malloc(sizeof(char) * (len + 1));
-    if (buf)
-    {
-        while (s[outside_index])
-        {
-            buf[outside_index] = (*f)(outside_index, s[outside_index]);
-            outside_index++;
-        }
-        buf[outside_index] = '\0';
-    }
-    return (buf);
+	outside_index = 0;
+	len = ft_strlen(s);
+	buf = (char *)malloc(sizeof(char) * (len + 1));
+	if (buf)
+	{
+		while (s[outside_index])
+		{
+			buf[outside_index] = (*f)(outside_index, s[outside_index]);
+			outside_index++;
+		}
+		buf[outside_index] = '\0';
+	}
+	return (buf);
 }
