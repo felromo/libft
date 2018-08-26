fdcb6408238fcf145705547244ca92139751a37e
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 23:51:07 -0700

    norm formatting ft_strncmp.c

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 23:50:18 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 23:50:42 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <libft.h>
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-  while (n-- && (unsigned char)*s1 == (unsigned char)*s2)
-    {
-      if (!*s1)
-        break;
-      s1++;
-      s2++;
-    }
-  return ((unsigned char)*s1 - (unsigned char)*s2);
+	while (n-- && (unsigned char)*s1 == (unsigned char)*s2)
+	{
+		if (!*s1)
+			break ;
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
