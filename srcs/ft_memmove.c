d2c1deabc180af3d09f113dbc2c0b43bee146538
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 22:48:24 -0700

    norm formatting

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -1,16 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 22:47:53 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 22:47:55 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-  char *tmpdst = (char *)dst;
-  char *tmpsrc = (char *)src;
-  size_t i = 0;
+	char		*tmpdst;
+	char		*tmpsrc;
+	size_t		i;
 
+	tmpdst = (char *)src;
+	tmpsrc = (char *)dst;
+	i = 0;
 	if (dst >= src)
-    while (len--)
-      tmpdst[len] = tmpsrc[len];
+		while (len--)
+			tmpdst[len] = tmpsrc[len];
 	else
-    while (i++ < len)
-      tmpdst[i - 1] = tmpsrc[i - 1];
+		while (i++ < len)
+			tmpdst[i - 1] = tmpsrc[i - 1];
 	return (dst);
 }
