74c0d490e67cd13a0ea0febc865209011a38fd0e
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 22:49:29 -0700

    norm formatting

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 22:49:13 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 22:49:14 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-void    *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (n--)
-    {
-        if ((unsigned char)c == ((unsigned char *)s)[i])
-            return ((void *)s + i);
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	while (n--)
+	{
+		if ((unsigned char)c == ((unsigned char *)s)[i])
+			return ((void *)s + i);
+		i++;
+	}
+	return (NULL);
 }
