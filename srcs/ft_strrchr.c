3fb1e1b8a38751682e27ae5507f865530a6b4a75
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 23:45:17 -0700

    norm formatting

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 23:43:43 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 23:44:05 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <libft.h>
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-  int len;
+	int len;
 
-  len = ft_strlen(s);
-  while (len)
-    {
-      if (s[len] == (char) c)
-        return ((char *)&(s[len]));
-      len--;
-    }
-  if (!c)
-    return ((char *)&(s[len]));
-  return (NULL);
+	len = ft_strlen(s);
+	while (len)
+	{
+		if (s[len] == (char)c)
+			return ((char *)&(s[len]));
+		len--;
+	}
+	if (!c)
+		return ((char *)&(s[len]));
+	return (NULL);
 }
