4ca73def2a15ba77e46255436918707ec4d901c1
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-25 22:54:54 -0700

    norm formatting

--- srcs/ft_strcpy.c
+++ srcs/ft_strcpy.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 22:54:34 by fromo             #+#    #+#             */
+/*   Updated: 2018/08/25 22:54:36 by fromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 
-char    *ft_strcpy(char *dst, const char *src)
+char	*ft_strcpy(char *dst, const char *src)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (src[i])
-    {
-        ((char *)dst)[i] = ((char *)src)[i];
-        i++;
-    }
-    dst[i] = '\0';
-    return ((char *)dst);
+	i = 0;
+	while (src[i])
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return ((char *)dst);
 }
