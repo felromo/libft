807694e3e9bbd880979a14ae9b5a7a8238d03de3
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-27 21:50:34 -0700

    passes moulitest

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 23:50:18 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/25 23:50:42 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/27 21:49:32 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- && (unsigned char)*s1 == (unsigned char)*s2)
+	if (!n)
+		return (0);
+	while (--n && (unsigned char)*s1 == (unsigned char)*s2)
 	{
 		if (!*s1)
 			break ;
