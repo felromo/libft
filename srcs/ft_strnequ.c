83228914fba92d8d18f6a0e0ae72821ea042dfaa
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-27 20:51:39 -0700

    passes moulitest

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -6,20 +6,23 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 00:25:39 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/27 18:59:16 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/27 20:50:32 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <libft.h>
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1)
 		return (0);
-	while (*s1 && *s2 && n--)
+	if (ft_strequ("", s2))
+		return (1);
+	while (n-- && *s1 && *s2)
 	{
 		if (*s1++ != *s2++)
 			return (0);
 	}
-	return (*s1 == *s2);
+	return (*--s1 == *--s2);
 }
