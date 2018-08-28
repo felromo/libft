05718f265ea29981f8f5c852f1fd167216db2727
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-28 02:45:09 -0700

    adds ft_isascii

--- includes/libft.h
+++ includes/libft.h
@@ -51,6 +51,7 @@ int					ft_atoi(const char *str);
 int					ft_isdigit(int c);
 int					ft_isalpha(int c);
 int					ft_isalnum(int c);
+int					ft_isascii(int c);
 int					ft_isprint(int c);
 int					ft_isupper(int c);
 int					ft_islower(int c);
