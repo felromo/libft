a5ae307392b7745e9accd1e2e91a502f13de89c4
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-27 23:09:32 -0700

    passes moulitest and norm formatting

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,11 +6,12 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 23:47:48 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/25 23:48:14 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/27 23:08:51 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <libft.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -20,19 +21,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int		i;
 
 	i = 0;
+	if (!*haystack)
+		return (NULL);
 	if (!*needle || haystack == needle)
 		return (char *)haystack;
-	while ((len - i) && *haystack)
+	while ((len - i) >= ft_strlen(needle) && *haystack)
 	{
 		cmp = (char *)haystack;
 		tmp = (char *)needle;
 		ltmp = len;
-		while (ltmp && *tmp && *cmp == *tmp)
-		{
-			cmp++;
+		while (ltmp-- && *tmp && *cmp++ == *tmp)
 			tmp++;
-			ltmp--;
-		}
 		if (ltmp > 0 && !*tmp)
 			return (char *)haystack;
 		haystack++;
