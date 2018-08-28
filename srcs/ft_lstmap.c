fc3d3ac929363c1faedbcdc70b9738f20ba02fbe
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-28 01:10:14 -0700

    passes moulitest unit-test and norm format

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/27 19:26:33 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/28 01:04:37 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/28 01:09:12 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,47 +24,8 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	head = body;
 	while ((lst = lst->next))
 	{
-		/* head->next = f(lst); */
-		/* head = head->next; */
-		head = (head->next = f(lst)) ? head->next : NULL;
+		head->next = f(lst);
+		head = head->next;
 	}
 	return (body);
 }
-
-/* t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem)) */
-/* { */
-/* 	t_list	*head; */
-/* 	t_list	*body; */
-/*  */
-/* 	if (!lst || !f) */
-/* 		return (NULL); */
-/* 	body = f(lst); */
-/* 	head = body; */
-/* 	while (1) */
-/* 	{ */
-/* 		head->next = f(lst); */
-/* 		head = head->next; */
-/* 		lst = lst->next; */
-/* 		if (!lst) */
-/* 			break ; */
-/* 	} */
-/* 	return (body); */
-/* } */
-
-/* t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem)) */
-/* { */
-/* 	t_list	*head; */
-/* 	t_list	*body; */
-/*  */
-/* 	if (!lst || !f) */
-/* 		return (NULL); */
-/* 	body = ft_memalloc(sizeof(t_list)); */
-/* 	head = body; */
-/* 	while (lst) */
-/* 	{ */
-/* 		head = f(lst); */
-/* 		head = head->next; */
-/* 		lst = lst->next; */
-/* 	} */
-/* 	return (body); */
-/* } */
