b2399a82dc1a29743e1452dc2009560f1b6547e1
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-28 19:59:28 -0700

    remove restricted keyword

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 22:31:36 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/28 18:04:26 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/28 19:58:51 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,8 @@ typedef struct		s_list
 
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
-void				*ft_memcpy(void *restrict dst,
-						const void *restrict src, size_t n);
-void				*ft_memccpy(void *restrict dst,
-						const void *restrict src, int c, size_t n);
+void				*ft_memcpy(void dst, const void src, size_t n);
+void				*ft_memccpy(void dst, const void src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
