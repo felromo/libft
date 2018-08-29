418a75ed627ae2780bbc767991dce21f5f033077
Felipe Romo <fromo@e1z2r7p29.42.us.org>
2018-08-28 19:05:13 -0700

    passes 3

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: fromo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/26 00:25:39 by fromo             #+#    #+#             */
-/*   Updated: 2018/08/27 20:50:32 by fromo            ###   ########.fr       */
+/*   Updated: 2018/08/28 19:03:40 by fromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (!s1)
+	if (!s1 | !s2)
 		return (0);
 	if (ft_strequ("", s2))
 		return (1);
