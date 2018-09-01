a120a8b4ef465e2064a9b4d4876542bb4e50a8f8
Felipe Romo <felipe@fromo.me>
2018-08-31 21:47:28 -0500

    strnstr seg

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -13,34 +13,34 @@
 #include <string.h>
 #include <libft.h>
 
-/* char	*ft_strnstr(const char *haystack, const char *needle, size_t len) */
-/* { */
-/* 	char	*cmp; */
-/* 	char	*tmp; */
-/* 	int		ltmp; */
-/* 	int		i; */
-
-/* 	i = 0; */
-/* 	if (!*haystack) */
-/* 		return (NULL); */
-/* 	if (!*needle || haystack == needle) */
-/* 		return (char *)haystack; */
-/* 	while ((len - i) >= ft_strlen(needle) && *haystack) */
-/* 	{ */
-/* 		cmp = (char *)haystack; */
-/* 		tmp = (char *)needle; */
-/* 		ltmp = len; */
-/* 		while (*tmp && ltmp-- && *cmp++ == *tmp && tmp++) */
-/* 			; */
-/* 		if (ltmp > 0 && !*tmp) */
-/* 			return (char *)haystack; */
-/* 		haystack++; */
-/* 		i++; */
-/* 	} */
-/* 	return (NULL); */
-/* } */
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-  return strnstr(haystack, needle, len);
+	char	*cmp;
+	char	*tmp;
+	int		ltmp;
+	int		i;
+
+	i = 0;
+	if (!*haystack)
+		return (NULL);
+	if (!*needle || haystack == needle)
+		return (char *)haystack;
+	while ((len - i) >= ft_strlen(needle) && *haystack)
+	{
+		cmp = (char *)haystack;
+		tmp = (char *)needle;
+		ltmp = len;
+		while (*tmp && ltmp-- && *cmp++ == *tmp && tmp++)
+			;
+		if (ltmp > 0 && !*tmp)
+			return (char *)haystack;
+		haystack++;
+		i++;
+	}
+	return (NULL);
 }
+
+/* char	*ft_strnstr(const char *haystack, const char *needle, size_t len) */
+/* { */
+/*   return strnstr(haystack, needle, len); */
+/* } */
