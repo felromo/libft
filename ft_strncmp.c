92aec10a64e5f0b62bd4e7440e1176da96a895a1
Felipe Romo <felipe@fromo.me>
2018-08-31 22:00:51 -0500

    test ft_strncmp for seg

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,21 +13,21 @@
 #include <string.h>
 #include <libft.h>
 
-/* int	ft_strncmp(const char *s1, const char *s2, size_t n) */
-/* { */
-/* 	if (!n) */
-/* 		return (0); */
-/* 	while (--n && (unsigned char)*s1 == (unsigned char)*s2) */
-/* 	{ */
-/* 		if (!*s1) */
-/* 			break ; */
-/* 		s1++; */
-/* 		s2++; */
-/* 	} */
-/* 	return ((unsigned char)*s1 - (unsigned char)*s2); */
-/* } */
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-  return strncmp(s1, s2, n);
+	if (!n)
+		return (0);
+	while (--n && (unsigned char)*s1 == (unsigned char)*s2)
+	{
+		if (!*s1)
+			break ;
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
+
+/* int	ft_strncmp(const char *s1, const char *s2, size_t n) */
+/* { */
+/*   return strncmp(s1, s2, n); */
+/* } */
