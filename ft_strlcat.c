21934df26b38f9ffd03798fcd2bb0dcbef99ba1a
Felipe Romo <felipe@fromo.me>
2018-08-31 21:36:31 -0500

    test ft_strlcat for seg

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -13,36 +13,36 @@
 #include <string.h>
 #include <libft.h>
 
-/* size_t	ft_strlcat(char *dst, const char *src, size_t dstsize) */
-/* { */
-/* 	char		*d; */
-/* 	const char	*s; */
-/* 	size_t		n; */
-/* 	size_t		dlen; */
-
-/* 	d = dst; */
-/* 	s = src; */
-/* 	n = dstsize; */
-/* 	while (n-- != 0 && *d != '\0') */
-/* 		d++; */
-/* 	dlen = d - dst; */
-/* 	n = dstsize - dlen; */
-/* 	if (n == 0) */
-/* 		return (dlen + ft_strlen(s)); */
-/* 	while (*s != '\0') */
-/* 	{ */
-/* 		if (n != 1) */
-/* 		{ */
-/* 			*d++ = *s; */
-/* 			n--; */
-/* 		} */
-/* 		s++; */
-/* 	} */
-/* 	*d = '\0'; */
-/* 	return (dlen + (s - src)); */
-/* } */
-
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-  return strlcat(dst, src, dstsize);
+	char		*d;
+	const char	*s;
+	size_t		n;
+	size_t		dlen;
+
+	d = dst;
+	s = src;
+	n = dstsize;
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = dstsize - dlen;
+	if (n == 0)
+		return (dlen + ft_strlen(s));
+	while (*s != '\0')
+	{
+		if (n != 1)
+		{
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+	return (dlen + (s - src));
 }
+
+/* size_t	ft_strlcat(char *dst, const char *src, size_t dstsize) */
+/* { */
+/*   return strlcat(dst, src, dstsize); */
+/* } */
