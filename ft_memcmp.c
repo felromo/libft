3502aebcea4470c778c499e29d507677cbd9e9c3
Felipe Romo <felipe@fromo.me>
2018-08-31 21:08:39 -0500

    dont use original for ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -13,21 +13,21 @@
 #include <stdlib.h>
 #include <libft.h>
 
-/* int	ft_memcmp(const void *s1, const void *s2, size_t n) */
-/* { */
-/* 	int i; */
-
-/* 	i = 0; */
-/* 	while (n--) */
-/* 	{ */
-/* 		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i]) */
-/* 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]); */
-/* 		i++; */
-/* 	} */
-/* 	return (0); */
-/* } */
-
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-  return memcmp(s1, s2, n);
+	int i;
+
+	i = 0;
+	while (n--)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+	}
+	return (0);
 }
+
+/* int	ft_memcmp(const void *s1, const void *s2, size_t n) */
+/* { */
+/*   return memcmp(s1, s2, n); */
+/* } */
