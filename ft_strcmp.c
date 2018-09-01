5a89e95f0f564fad373ec2771d525bc97d2cc73c
Felipe Romo <felipe@fromo.me>
2018-08-31 21:58:16 -0500

    test ft_strcmp for seg

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -12,19 +12,19 @@
 
 #include <string.h>
 
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	while ((unsigned char)*s1 == (unsigned char)*s2)
-	{
-		if (!*s1)
-			break ;
-		s1++;
-		s2++;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
-
 /* int	ft_strcmp(const char *s1, const char *s2) */
 /* { */
-/*   return strcmp(s1, s2); */
+/* 	while ((unsigned char)*s1 == (unsigned char)*s2) */
+/* 	{ */
+/* 		if (!*s1) */
+/* 			break ; */
+/* 		s1++; */
+/* 		s2++; */
+/* 	} */
+/* 	return ((unsigned char)*s1 - (unsigned char)*s2); */
 /* } */
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+  return strcmp(s1, s2);
+}
