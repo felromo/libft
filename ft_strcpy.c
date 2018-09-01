ff82b6ced665cb8911d4b028b5c652ecb2415e7f
Felipe Romo <felipe@fromo.me>
2018-08-31 21:30:04 -0500

    test ft_strcpy for seg

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -13,21 +13,21 @@
 #include <libft.h>
 #include <string.h>
 
-/* char	*ft_strcpy(char *dst, const char *src) */
-/* { */
-/* 	int i; */
-
-/* 	i = 0; */
-/* 	while (src[i]) */
-/* 	{ */
-/* 		((char *)dst)[i] = ((char *)src)[i]; */
-/* 		i++; */
-/* 	} */
-/* 	dst[i] = '\0'; */
-/* 	return ((char *)dst); */
-/* } */
-
 char	*ft_strcpy(char *dst, const char *src)
 {
-  return strcpy(dst, src);
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return ((char *)dst);
 }
+
+/* char	*ft_strcpy(char *dst, const char *src) */
+/* { */
+/*   return strcpy(dst, src); */
+/* } */
